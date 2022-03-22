bd0985e99833daaf78ad7fa69676c39f024a17ed
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-03-22 22:13:35 +0200

    Added the regex check functions

--- main.cpp
+++ main.cpp
@@ -3,7 +3,34 @@
 
 using namespace std;
 
-// get index of character in char array , string
+int indexOf(string arr, char character);
+bool checkIfPlusMinus(string complexNumber);
+bool checkIfMultiplyDivide(string complexNumber);
+
+int main() {
+
+    string complexNum = "(4421142-978883i)/(4421142+978883i)";
+
+//    int x = complexNum.find("-");
+//
+//
+//    string real = complexNum.substr(0, x);
+//
+//    cout << real << endl;
+//
+//    string imag = complexNum.substr(x, indexOf(complexNum, 'i') - x);
+//
+//    cout << imag << endl;
+
+    if (checkIfPlusMinus(complexNum)){
+        cout << "Plus Minus";
+    }else if(checkIfMultiplyDivide(complexNum)){
+        cout << "Multiply divide";
+    }
+
+    return 0;
+}
+
 int indexOf(string arr, char character) {
 
     for (int i = 0; i < arr.length(); i++) {
@@ -17,27 +44,16 @@ int indexOf(string arr, char character) {
     return 0;
 }
 
-int main() {
-
-    string complexNum = "(4421142-978883i)+(4421142+978883i)";
+bool checkIfPlusMinus(string complexNumber){
 
     regex complexCheckPlusMinus ("[(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)][+-][(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)]");
+
+    return regex_match(complexNumber, complexCheckPlusMinus);
+}
+
+bool checkIfMultiplyDivide(string complexNumber){
+
     regex complexCheckMultiplyDivide ("[(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)][*/][(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)]");
 
-    cout << regex_match(complexNum, complexCheckPlusMinus);
-    cout << regex_match(complexNum, complexCheckMultiplyDivide);
-
-//    int x = complexNum.find("-");
-//
-//
-//    string real = complexNum.substr(0, x);
-//
-//    cout << real << endl;
-//
-//    string imag = complexNum.substr(x, indexOf(complexNum, 'i') - x);
-//
-//    cout << imag << endl;
-
-
-    return 0;
+    return regex_match(complexNumber, complexCheckMultiplyDivide);
 }
