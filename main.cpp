6ae2dc61099980c604bdb46fa52666a4074fcf1d
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-03-22 18:31:01 +0200

    Added the regex

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <regex>
+
 using namespace std;
+
+// get index of character in char array , string
+int indexOf(string arr, char character) {
+
+    for (int i = 0; i < arr.length(); i++) {
+
+        if (character == arr[i]) {
+            return i;
+        }
+
+    }
+
+    return 0;
+}
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+
+    string complexNum = "(4421142-978883i)+(4421142+978883i)";
+
+    regex complexCheckPlusMinus ("[(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)][+-][(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)]");
+    regex complexCheckMultiplyDivide ("[(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)][*/][(][+-]*[0-9]+(.[0-9]+)*[+-]+[0-9]+i[)]");
+
+    cout << regex_match(complexNum, complexCheckPlusMinus);
+    cout << regex_match(complexNum, complexCheckMultiplyDivide);
+
+//    int x = complexNum.find("-");
+//
+//
+//    string real = complexNum.substr(0, x);
+//
+//    cout << real << endl;
+//
+//    string imag = complexNum.substr(x, indexOf(complexNum, 'i') - x);
+//
+//    cout << imag << endl;
+
+
     return 0;
 }
