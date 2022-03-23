b34d822aa3f25c3fc3c88c8629224bfd9ced24d8
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-03-24 00:35:17 +0200

    Updated output style. Added author, contributors names.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 Program Name: Complex Number Calculator
 Description: This program takes complex numbers in the form "(a+bi)+(c+di)" and calculates the entered problem.
 Purpose: Make operations on complex numbers
-Author: Youssef Moataz
-Last Modification Date: March 23, 2022.
-Version: v1.4.0
+Author and contributors: Youssef Moataz, Aya Ali, Hend Ahmed
+IDs: 20210488, 20210083, 20211111
+Group: A
+Labs: S4, S4, S9
+Teaching Assistant: -
+Last Modification Date: March 24, 2022.
+Version: v1.5.0
 
 */
 
@@ -36,9 +40,9 @@ void add(double real1, double imag1, double real2, double imag2);
 
 void sub(double real1, double imag1, double real2, double imag2);
 
-void multiplication(double &real1, double &real2, double &imag1,double &imag2);
+void multiplication(double real1, double real2, double imag1, double imag2);
 
-void division(double &real1, double &real2, double &imag1,double &imag2);
+void division(double real1, double real2, double imag1, double imag2);
 
 //string complexNum = "(-4421142-978883i)*(4421142+978883i)";
 //string complexNum2 = "(42+9i)+(-404+971i)";
@@ -73,7 +77,7 @@ void getInput() {
 
         extractNumbersIfMultiplyDivide(userComplexNumber);
 
-    } else if (userComplexNumber == "Exit"){
+    } else if (userComplexNumber == "Exit" || userComplexNumber == "exit") {
         exit(0);
     }
 
@@ -142,8 +146,6 @@ void extractNumbersIfPlusMinus(string complexNumber) {
             getFirstPartNumbers(firstPart, real1, imag1);
             getSecondPartNumbers(secondPart, real2, imag2);
 
-            // subtraction function here
-
             sub(real1, imag1, real2, imag2);
 
         }
@@ -161,8 +163,6 @@ void extractNumbersIfPlusMinus(string complexNumber) {
 
         getFirstPartNumbers(firstPart, real1, imag1);
         getSecondPartNumbers(secondPart, real2, imag2);
-
-        // addition function here
 
         add(real1, imag1, real2, imag2);
 
@@ -201,7 +201,7 @@ void extractNumbersIfMultiplyDivide(string complexNumber) {
             getFirstPartNumbers(firstPart, real1, imag1);
             getSecondPartNumbers(secondPart, real2, imag2);
 
-            division(real1,real2,imag1,imag2);
+            division(real1, real2, imag1, imag2);
 
         }
 
@@ -219,7 +219,7 @@ void extractNumbersIfMultiplyDivide(string complexNumber) {
         getFirstPartNumbers(firstPart, real1, imag1);
         getSecondPartNumbers(secondPart, real2, imag2);
 
-        multiplication(real1,real2,imag1,imag2);
+        multiplication(real1, real2, imag1, imag2);
 
     }
 
@@ -238,8 +238,8 @@ void getFirstPartNumbers(string firstPart, double &real1, double &imag1) {
         string real = firstPart.substr(0, signIndex);
         string imag = firstPart.substr(signIndex + 1, indexOf(firstPart, 'i') - signIndex - 1);
 
-        cout << real << endl;
-        cout << imag << endl;
+//        cout << real << endl;
+//        cout << imag << endl;
 
         real1 = stod(real);
         imag1 = stod(imag);
@@ -249,8 +249,8 @@ void getFirstPartNumbers(string firstPart, double &real1, double &imag1) {
         string real = firstPart.substr(0, signIndex);
         string imag = firstPart.substr(signIndex, indexOf(firstPart, 'i') - signIndex);
 
-        cout << real << endl;
-        cout << imag << endl;
+//        cout << real << endl;
+//        cout << imag << endl;
 
         real1 = stod(real);
         imag1 = stod(imag);
@@ -272,8 +272,8 @@ void getSecondPartNumbers(string secondPart, double &real2, double &imag2) {
         string real = secondPart.substr(0, signIndex);
         string imag = secondPart.substr(signIndex + 1, indexOf(secondPart, 'i') - signIndex - 1);
 
-        cout << real << endl;
-        cout << imag << endl;
+//        cout << real << endl;
+//        cout << imag << endl;
 
         real2 = stod(real);
         imag2 = stod(imag);
@@ -283,8 +283,8 @@ void getSecondPartNumbers(string secondPart, double &real2, double &imag2) {
         string real = secondPart.substr(0, signIndex);
         string imag = secondPart.substr(signIndex, indexOf(secondPart, 'i') - signIndex);
 
-        cout << real << endl;
-        cout << imag << endl;
+//        cout << real << endl;
+//        cout << imag << endl;
 
         real2 = stod(real);
         imag2 = stod(imag);
@@ -298,40 +298,51 @@ void add(double real1, double imag1, double real2, double imag2) {
     double real = (real1 + real2);
     double imag = (imag1 + imag2);
 
-    if (imag < 0){
-        cout << real << imag << "i" << endl;
-    }else{
-        cout << real << "+" << imag << "i" << endl;
+    if (imag < 0) {
+        cout << " = (" << real << imag << "i" << ")" << endl;
+    } else {
+        cout << " = (" << real << "+" << imag << "i" << ")" << endl;
     }
 
 }
 
 void sub(double real1, double imag1, double real2, double imag2) {
+
     double real = (real1 - real2);
     double imag = (imag1 - imag2);
 
-    if (imag < 0){
-        cout << real << imag << "i" << endl;
-    }else{
-        cout << real << "+" << imag << "i" << endl;
+    if (imag < 0) {
+        cout << " = (" << real << imag << "i" << ")" << endl;
+    } else {
+        cout << " = (" << real << "+" << imag << "i" << ")" << endl;
     }
 
 }
-void multiplication(double &real1, double &real2, double &imag1,double &imag2){
-    double real=(real1*real2)-(imag1*imag2);
-    double imag=(real1*imag2)+(real2*imag1);
-    if(imag>0){
-        cout<<real<<"+"<<imag<<"i"<<endl;
-    }else{
-        cout<<real<<imag<<"i"<<endl;
+
+void multiplication(double real1, double real2, double imag1, double imag2) {
+
+    double real = (real1 * real2) - (imag1 * imag2);
+    double imag = (real1 * imag2) + (real2 * imag1);
+
+    if (imag > 0) {
+        cout << " = (" << real << "+" << imag << "i" << ")" << endl;
+    } else {
+        cout << " = (" << real << imag << "i" << ")" << endl;
     }
+
 }
-void division(double &real1, double &real2, double &imag1,double &imag2){
-    double real=((real1*real2)+(imag1*imag2))/((real2*real2)+(imag2*imag2));
-    double imag=(((real1*(-imag2))+(imag1*real2))/(((real2*real2))+(imag2*imag2)));
-    if(imag>0){
-        cout<<real<<"+"<<imag<<"i"<<endl;
-    }else{
-        cout<<real<<imag<<"i"<<endl;
+
+void division(double real1, double real2, double imag1, double imag2) {
+
+    double real = ((real1 * real2) + (imag1 * imag2)) / ((real2 * real2) + (imag2 * imag2));
+    double imag = (((real1 * (-imag2)) + (imag1 * real2)) / (((real2 * real2)) + (imag2 * imag2)));
+
+    if (imag > 0) {
+        cout << " = (" << real << "+" << imag << "i" << ")" << endl;
+    } else {
+        cout << " = (" << real << imag << "i" << ")" << endl;
     }
+
 }
+
+// End of the file
